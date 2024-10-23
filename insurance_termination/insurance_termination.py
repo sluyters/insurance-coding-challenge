8d2b32fe343b5eafcfec97d7bf210539696198e8
Arthur Sluÿters <26011209+sluyters@users.noreply.github.com>
2024-10-23 21:25:15 +0200

    Added unit tests, fixed code to support edge edge cases

--- insurance_termination/insurance_termination.py
+++ insurance_termination/insurance_termination.py
@@ -4,6 +4,15 @@ import argparse
 from datetime import datetime, date
 from dateutil.relativedelta import relativedelta
 
+# Error classes
+class UnsupportedDateError(ValueError):
+  """Raise when a date falls out of the scope of a function."""
+
+
+class ContractDateError(ValueError):
+  """Raise when a contract starting date is invalid."""
+
+
 # Main function
 def get_earliest_standard_termination(
     contract_start: date,
@@ -16,7 +25,7 @@ def get_earliest_standard_termination(
   This function applies only to non-life insurance contracts concluded by consumers that started or were renewed after April 4, 2014. It does not handle exceptions such as termination following a claim, a non-payment, or a rise in the premium.
 
   Args:
-    contract_start: A date object representing the starting date of the insurance contract, i.e., when the contract with the insurance broker first took effect.
+    contract_start: A date object representing the starting date of the insurance contract, i.e., when the contract with the insurer first took effect.
     earliest_expected_termination: A date object representing the earliest termination date wanted by the policy holder (defaults to the same date as the reference_date argument if ignored or smaller than the reference_date).
     reference_date: A date object representing the earliest possible date at which notice can be given to the insurer for contract termination (defaults to today's date).
 
@@ -24,83 +33,118 @@ def get_earliest_standard_termination(
     date: A date object representing the earliest possible termination date of the insurance contract.
 
   Raises:
-    DateError: If the latest contract renewal date falls outside of the supported range. 
+    UnsupportedDateError: If earliest_expected_termination is before April 4, 2014. 
+    ContractDateError: If the starting date of a contract does not preceed the reference date. 
   """
-
   # Set default values if arguments are not provided
   reference_date = reference_date or date.today()
   if not earliest_expected_termination or earliest_expected_termination < reference_date:
     earliest_expected_termination = reference_date
 
-  # Get the date of the latest renewal before the earliest expected termination date
-  contract_age = relativedelta(earliest_expected_termination, contract_start)
-  latest_renewal = contract_start + relativedelta(years=contract_age.years)
-
-  # Check which rules should be applied based on the latest renewal date
-  if latest_renewal >= date(2024, 10, 1):
-    return get_earliest_standard_termination_from_10_2024_law(contract_start, latest_renewal, earliest_expected_termination, reference_date,)
-  elif latest_renewal >= date(2014, 4, 4):
-    return get_earliest_standard_termination_from_04_2014_law(latest_renewal, earliest_expected_termination, reference_date)
+  # Check validity of the parameters
+  if reference_date < contract_start:
+    raise ContractDateError(f'The starting date of a contract (contract_start = {contract_start}) should preceed the reference date (reference_date = {reference_date}).')
+  if earliest_expected_termination < date(2014, 4, 4):
+    raise UnsupportedDateError(f'The "earliest_expected_termination" parameter ({earliest_expected_termination}) falls outside of the supported range (2014/04/04 - now).')
+  
+  # Compute earliest contract termination date
+  contract_age_at_expected_termination = relativedelta(earliest_expected_termination, contract_start)
+  if contract_start >= date(2024, 10, 1):
+    # Contract started after new law from October 1st, 2024
+    earliest_notice_end = reference_date + relativedelta(months=2)
+    first_contract_renewal = contract_start + relativedelta(years=1)
+    if ceil_years(contract_age_at_expected_termination) <= 1 and earliest_notice_end < first_contract_renewal:
+      # Case 1 - Before the first anniversary of a contract, terminate at the end of the contract with 2 months notice
+      return get_earliest_termination_case_1(contract_start)
+    else:
+      # Case 2 - After first automatic renewal, terminate at any time with 2 months notice
+      return get_earliest_termination_case_2(earliest_expected_termination, reference_date, first_contract_renewal)
   else:
-    raise UnsupportedDateError(f'Latest contract renewal date ({latest_renewal}) falls outside of the supported range (2014/04/04 - now).')
-
+    # Contract started before new law from October 1st, 2024
+    earliest_notice_end = reference_date + relativedelta(months=3)
+    next_contract_renewal = contract_start + relativedelta(years=ceil_years(contract_age_at_expected_termination))
+    previous_contract_renewal = next_contract_renewal - relativedelta(years=1)
+    if previous_contract_renewal >= date(2024, 10, 1):
+      # Case 2
+      return get_earliest_termination_case_2(earliest_expected_termination, reference_date, previous_contract_renewal)
+    elif next_contract_renewal >= date(2024, 10, 1) and earliest_notice_end > next_contract_renewal:
+      # Case 2
+      return get_earliest_termination_case_2(earliest_expected_termination, reference_date, next_contract_renewal)
+    else:
+      # Case 3 - Terminate at the end of the contract with 3 months notice
+      return get_earliest_termination_case_3(earliest_notice_end, next_contract_renewal)
+ 
 
 # Helpers
-class UnsupportedDateError(ValueError):
-  """Raise when a date falls out of the scope of a function."""
-
-
-def get_earliest_standard_termination_from_10_2024_law(
-    contract_start: date, 
-    latest_renewal: date, 
-    earliest_expected_termination: date,
-    reference_date: date
+def get_earliest_termination_case_1(
+    contract_start: date
 ) -> date:
   """
-    Provides the earliest possible termination date of a non-life insurance contract started or last renewed after October 1st, 2024. 
+  Provides the earliest termination date of a contract signed after October 1st, 2024, before its first automatic renewal.
 
-    Args:
-      contract_start: A date object representing the starting date of the insurance contract, i.e., when the contract with the insurance broker first took effect.
-      latest_renewal: A date object representing the latest renewal of the insurance contract.
-      earliest_expected_termination: A date object representing the earliest termination date wanted by the policy holder/provider.
-      reference_date: A date object representing the earliest possible date at which notice can be given to the insurer for contract termination.
-      
-    Returns:
-      A date object representing the earliest possible termination date of the insurance contract.
+  Args:
+    contract_start: A date object representing the starting date of the insurance contract, i.e., when the contract with the insurer first took effect.
+
+  Returns:
+    date: A date object representing the earliest possible termination date of the insurance contract.
   """
+  return contract_start + relativedelta(years=1)
 
-  earliest_notice_end = reference_date + relativedelta(months=2)
-  if contract_start == latest_renewal:
-    # Within the first year of the contract, terminate at the end of the contract with 2 months notice
-    earliest_possible_termination = latest_renewal + relativedelta(years=1)
-    return max(earliest_possible_termination, earliest_expected_termination, earliest_notice_end)
+
+def get_earliest_termination_case_2(
+    earliest_expected_termination: date,
+    reference_date: date,
+    first_contract_renewal: date
+) -> date:
+  """
+  Provides the earliest termination date of a contract signed or renewed after October 1st, 2024, after its first automatic renewal.
+
+  Args:
+    earliest_expected_termination: A date object representing the earliest termination date wanted by the policy holder.
+    reference_date: A date object representing the earliest possible date at which notice can be given to the insurer for contract termination.
+    first_contract_renewal: A date object representing the date of the first automatic renewal of the contract after October 1st, 2024.
+
+  Returns:
+    date: A date object representing the earliest possible termination date of the insurance contract.
+  """
+  earliest_notice_end = max(reference_date, first_contract_renewal) + relativedelta(months=2)
+  return max(earliest_expected_termination, earliest_notice_end)
+
+
+def get_earliest_termination_case_3(
+    earliest_notice_end: date,
+    next_contract_renewal: date
+) -> date:
+  """
+  Provides the earliest termination date of a contract last renewed before October 1st, 2024.
+
+  Args:
+    earliest_notice_end: The earliest date at which the notice given to the insurer ends.
+    next_contract_renewal: The date of the next renewal of the insurance contract.
+  
+  Returns:
+    date: A date object representing the earliest possible termination date of the insurance contract.
+  """
+  if earliest_notice_end > next_contract_renewal:
+    return next_contract_renewal + relativedelta(years=1)
   else:
-    # After renewal, terminate at any time with 2 months notice
-    return max(earliest_expected_termination, earliest_notice_end)
-  
+    return next_contract_renewal
 
-def get_earliest_standard_termination_from_04_2014_law(
-    latest_renewal: date, 
-    earliest_expected_termination: date,
-    reference_date: date
-) -> date:
+
+def ceil_years(delta: relativedelta) -> int:
+  """Rounds up the number of years in a relativedelta object.
+
+  Args:
+    delta: A relativedelta object. 
+
+  Returns: 
+    int: The number of years in the delta parameter rounded up.
   """
-    Provides the earliest possible termination date of a non-life insurance contract started or last renewed after April 4, 2014 and before September 30, 2024. 
-
-    Args:
-      latest_renewal: A date object representing the latest renewal of the insurance contract.
-      earliest_expected_termination: A date object representing the earliest termination date wanted by the policy holder/provider.
-      reference_date: A date object representing the earliest possible date at which notice can be given to the insurer for contract termination.
-
-    Returns:
-      A date object representing the earliest possible termination date of the insurance contract.
-  """
+  if delta.days > 0 or delta.months > 0:
+    return delta.years + 1
+  else:
+    return delta.years
   
-  # Terminate at the end of the contract with 3 months notice
-  earliest_notice_end = reference_date + relativedelta(months=3)
-  earliest_possible_termination = latest_renewal + relativedelta(years=1)
-  return max(earliest_possible_termination, earliest_expected_termination, earliest_notice_end)
-
 
 def parse_date(string_date: str) -> date:
   """
@@ -115,7 +159,6 @@ def parse_date(string_date: str) -> date:
   Raises:
     argparse.ArgumentTypeError: If string_date could not be parsed into a valid date object. 
   """
-
   try:
     return datetime.strptime(string_date, '%Y-%m-%d').date()
   except ValueError:
@@ -126,7 +169,7 @@ def parse_date(string_date: str) -> date:
 if __name__ == '__main__':
   # Define program arguments
   parser = argparse.ArgumentParser('insurance_termination', description='This program provides the earliest possible termination date of a non-life insurance contract based on the starting date of the contract, the earliest possible starting date of the notice period (today if not specified), and the earliest expected termination date of the contract. It applies only to non-life insurance contracts concluded by consumers that started or were renewed after April 4, 2014.')
-  parser.add_argument('contractstart', help='The starting date of the insurance contract, i.e., when the contract with the insurance broker first took effect. Format: YYYY-MM-DD.', type=parse_date)
+  parser.add_argument('contractstart', help='The starting date of the insurance contract, i.e., when the contract with the insurer first took effect. Format: YYYY-MM-DD.', type=parse_date)
   parser.add_argument('-t', '--earliestexpectedtermination', help='The earliest termination date (YYYY-MM-DD) wanted by the policy holder. Format: YYYY-MM-DD.', type=parse_date, default=None)
   parser.add_argument('-r', '--referencedate', help='The first possible date at which notice can be given to the policy holder/provider for contract termination. Format: YYYY-MM-DD.', type=parse_date, default=None)
   # Parse arguments & run function
--- tests/test_insurance_termination.py
+++ tests/test_insurance_termination.py
@@ -1,12 +1,14 @@
 import unittest
 import argparse
 from datetime import date
+from dateutil.relativedelta import relativedelta
 import unittest.mock
 from insurance_termination import insurance_termination
 
+
 class TestParseDate(unittest.TestCase):
   """
-  Test the parse_date function.
+  Test the parse_date function of the insurance_termination module.
   """
 
   def test_correct_date(self):
@@ -32,39 +34,127 @@ class TestParseDate(unittest.TestCase):
     self.assertRaises(argparse.ArgumentTypeError, insurance_termination.parse_date, '99-12-29')
 
 
+class TestCeilYears(unittest.TestCase):
+  """
+  Test the ceil_years function of the insurance_termination module.
+  """
+
+  def test_round_year(self):
+    result = insurance_termination.ceil_years(relativedelta(years=1))
+    self.assertEqual(result, 1)
+
+  def test_year_with_month(self):
+    result = insurance_termination.ceil_years(relativedelta(years=1, months=6))
+    self.assertEqual(result, 2)
+
+  def test_year_with_day(self):
+    result = insurance_termination.ceil_years(relativedelta(years=1, days=20))
+    self.assertEqual(result, 2)
+
+  def test_year_with_day_and_month(self):
+    result = insurance_termination.ceil_years(relativedelta(years=1, months=4, days=6))
+    self.assertEqual(result, 2)
+
+
+class TestEarliestTerminationCase1(unittest.TestCase):
+  """
+  Test the get_earliest_termination_case_1 function of the insurance_termination module.
+  """
+
+  def test_should_give_date_of_first_renewal(self):
+    result = insurance_termination.get_earliest_termination_case_1(date(2025, 10, 1))
+    self.assertEqual(result, date(2026, 10, 1))
+
+
+class TestEarliestTerminationCase2(unittest.TestCase):
+  """
+  Test the get_earliest_termination_case_2 function of the insurance_termination module.
+  """
+
+  def test_should_give_earliest_expected_termination_if_notice_ends_before(self):
+    result = insurance_termination.get_earliest_termination_case_2(date(2026, 10, 1), date(2026, 6, 1), date(2026, 7, 1))
+    self.assertEqual(result, date(2026, 10, 1))
+
+  def test_should_wait_for_2_months_notice_from_first_contract_renewal(self):
+    result = insurance_termination.get_earliest_termination_case_2(date(2026, 10, 1), date(2026, 6, 1), date(2026, 9, 1))
+    self.assertEqual(result, date(2026, 11, 1))
+
+  def test_should_wait_for_2_months_notice_from_reference_date(self):
+    result = insurance_termination.get_earliest_termination_case_2(date(2026, 10, 1), date(2026, 9, 1), date(2026, 6, 1))
+    self.assertEqual(result, date(2026, 11, 1))
+
+
+class TestEarliestTerminationCase3(unittest.TestCase):
+  """
+  Test the get_earliest_termination_case_3 function of the insurance_termination module.
+  """
+
+  def test_should_give_next_renewal_if_notice_ends_before(self):
+    result = insurance_termination.get_earliest_termination_case_3(date(2020, 10, 1), date(2020, 11, 1))
+    self.assertEqual(result, date(2020, 11, 1))
+
+  def test_should_skip_next_renewal_if_notice_ends_after(self):
+    result = insurance_termination.get_earliest_termination_case_3(date(2020, 12, 1), date(2020, 11, 1))
+    self.assertEqual(result, date(2021, 11, 1))
+
+
 class TestGetEarliestTermination(unittest.TestCase):
   """
-  Test the get_earliest_standard_termination function.
+  Test the get_earliest_standard_termination function of the insurance_termination module.
   """
 
   def test_should_throw_exception_if_last_renewal_is_before_01_04_2014(self):
     self.assertRaises(insurance_termination.UnsupportedDateError, insurance_termination.get_earliest_standard_termination, date(1999, 1, 1), date(2000, 1, 1), date(2000, 1, 1))
 
+  def test_should_throw_exception_if_contract_start_more_recent_than_reference_date(self):
+    self.assertRaises(insurance_termination.ContractDateError, insurance_termination.get_earliest_standard_termination, date(2020, 1, 1), date(2020, 1, 1), date(2018, 1, 1))
 
-  def test_should_wait_until_end_of_contract_for_contract_started_after_01_10_2024_within_first_year(self):
+
+  def test_should_wait_until_end_of_contract_for_contracts_started_after_01_10_2024_in_their_first_year(self):
     result = insurance_termination.get_earliest_standard_termination(date(2024, 10, 1), date(2025, 4, 1), date(2025, 3, 1))
     self.assertEqual(result, date(2025, 10, 1))
 
-  def test_should_require_2_months_notice_for_contract_started_after_01_10_2024_after_renewal_1(self):
-    result = insurance_termination.get_earliest_standard_termination(date(2024, 10, 1), date(2025, 9, 20), date(2025, 9, 20))
-    self.assertEqual(result, date(2025, 11, 20))
+  def test_should_require_2_months_notice_for_contracts_started_after_01_10_2024_in_their_first_year(self):
+    result = insurance_termination.get_earliest_standard_termination(date(2024, 10, 1), date(2025, 9, 1), date(2025, 9, 1))
+    self.assertEqual(result, date(2025, 12, 1))
 
-  def test_should_require_2_months_notice_for_contract_started_after_01_10_2024_after_renewal_2(self):
-    result = insurance_termination.get_earliest_standard_termination(date(2024, 10, 1), date(2026, 6, 25), date(2026, 4, 25))
-    self.assertEqual(result, date(2026, 6, 25))
+  def test_should_require_2_months_notice_for_contracts_started_after_01_10_2024_older_than_a_year(self):
+    result = insurance_termination.get_earliest_standard_termination(date(2024, 10, 1), date(2026, 9, 1), date(2026, 9, 1))
+    self.assertEqual(result, date(2026, 11, 1))
 
 
-  def test_should_wait_until_end_of_contract_for_old_contract_not_renewed_after_01_10_2024(self):
-    result = insurance_termination.get_earliest_standard_termination(date(2022, 5, 18), date(2024, 10, 1), date(2024, 10, 1))
+  def test_should_wait_until_end_of_contract_for_contracts_started_before_01_10_2024_not_renewed_after_01_10_2024(self):
+    result = insurance_termination.get_earliest_standard_termination(date(2018, 5, 18), date(2024, 10, 1), date(2024, 10, 1))
     self.assertEqual(result, date(2025, 5, 18))
 
-  def test_should_require_3_months_notice_for_old_contract_not_renewed_after_01_10_2024(self):
-    result = insurance_termination.get_earliest_standard_termination(date(2022, 5, 18), date(2025, 2, 20), date(2025, 2, 20))
-    self.assertEqual(result, date(2025, 5, 20))
+  def test_should_require_3_months_notice_for_contracts_started_before_01_10_2024_not_renewed_after_01_10_2024(self):
+    result = insurance_termination.get_earliest_standard_termination(date(2018, 5, 18), date(2025, 2, 20), date(2025, 2, 20))
+    self.assertEqual(result, date(2025, 7, 18))
 
-  def test_should_require_2_months_notice_for_old_contract_renewed_after_01_10_2024(self):
-    result = insurance_termination.get_earliest_standard_termination(date(2022, 5, 18), date(2025, 5, 21), date(2025, 5, 21))
+  def test_should_require_2_months_notice_for_contracts_started_before_01_10_2024_renewed_after_01_10_2024(self):
+    result = insurance_termination.get_earliest_standard_termination(date(2018, 5, 18), date(2025, 5, 21), date(2025, 5, 21))
     self.assertEqual(result, date(2025, 7, 21))
+
+  
+  def test_should_wait_until_end_of_contract_for_contracts_started_before_01_10_2024_with_3_months_notice_1(self):
+    result = insurance_termination.get_earliest_standard_termination(date(2016, 1, 1), date(2017, 1, 1), date(2016, 8, 31))
+    self.assertEqual(result, date(2017, 1, 1))
+
+  def test_should_wait_until_end_of_contract_for_contracts_started_before_01_10_2024_with_3_months_notice_2(self):
+    result = insurance_termination.get_earliest_standard_termination(date(2016, 1, 1), date(2016, 8, 1), date(2016, 8, 31))
+    self.assertEqual(result, date(2017, 1, 1))
+
+  def test_should_wait_until_next_renewal_for_contracts_started_before_01_10_2024_with_late_notice(self):
+    result = insurance_termination.get_earliest_standard_termination(date(2016, 1, 1), date(2016, 12, 31), date(2016, 12, 31))
+    self.assertEqual(result, date(2018, 1, 1))
+
+  def test_should_wait_until_next_renewal_after_earliest_expected_termination_for_contracts_started_before_01_10_2024_1(self):
+    result = insurance_termination.get_earliest_standard_termination(date(2016, 1, 1), date(2017, 1, 2), date(2016, 8, 31))
+    self.assertEqual(result, date(2018, 1, 1))
+
+  def test_should_wait_until_next_renewal_after_earliest_expected_termination_for_contracts_started_before_01_10_2024_2(self):
+    result = insurance_termination.get_earliest_standard_termination(date(2016, 1, 1), date(2022, 1, 2), date(2016, 8, 31))
+    self.assertEqual(result, date(2023, 1, 1))
 
 
   def test_should_handle_contracts_created_on_a_leap_day_before_01_10_2024(self):
